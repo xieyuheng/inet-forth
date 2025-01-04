92ae5e5e58a90a6ad0657b43cdf6c447ce45adce
Xie Yuheng <xyheme@gmail.com>
2025-01-04 20:47:00 +0800

    [lang] step_net -- call wire_destroy

--- src/vm/activity.h
+++ src/vm/activity.h
@@ -1,1 +1,6 @@
 #pragma once
+
+struct activity_t {
+    const wire_t *wire;
+    const rule_t *rule;
+};
--- src/vm/types.h
+++ src/vm/types.h
@@ -10,6 +10,7 @@ typedef enum {
 typedef struct mod_t mod_t;
 typedef struct function_t function_t;
 typedef struct frame_t frame_t;
+typedef struct activity_t activity_t;
 typedef struct vm_t vm_t;
 typedef struct rule_t rule_t;
 
