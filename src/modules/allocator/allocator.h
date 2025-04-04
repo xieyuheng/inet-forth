708420e37c178cd8418a6a98f500950842beb760
Xie Yuheng <xyheme@gmail.com>
2025-04-04 21:22:57 +0800

    up

--- src/modules/allocator/allocator.h
+++ src/modules/allocator/allocator.h
@@ -4,6 +4,9 @@
 // learned from section "6.4.3 Resource Allocator Caches"
 // of the book "Is Parallel Programming Hard?".
 
+// - allocate := pop  per-thread stack -- return value to caller
+// - free     := push per-thread stack -- recycle value from caller
+
 struct allocator_t {
     stack_t *main_stack;
 };
