aa4932d06dda2bde99911bf0b8b0605e6859fb1e
Xie Yuheng <xyheme@gmail.com>
2025-04-05 20:04:58 +0800

    up

--- src/modules/thread/atomics.h
+++ src/modules/thread/atomics.h
@@ -1,9 +1,5 @@
 #pragma once
 
-// This prevents the compiler from performing certain optimizations,
-// ensuring that the program always reads
-// the most up-to-date value of the variable.
-
 #define relaxed_load(pointer) \
     atomic_load_explicit(pointer, memory_order_relaxed)
 #define acquire_load(pointer) \
