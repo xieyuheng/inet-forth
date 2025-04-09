8f51023eabd8cbaf25cf80859b73ff6eab010b43
Xie Yuheng <xyheme@gmail.com>
2025-04-09 18:32:26 +0800

    up

--- src/net/node_allocator.c
+++ src/net/node_allocator.c
@@ -14,7 +14,7 @@ prepare_one_batch_of_nodes(node_allocator_t *self) {
 
     for (size_t i = 0; i < self->batch_size; i++) {
         node_t *node = node_new(NULL);
-        node->id = self->node_count++;
+        node->id = ++self->node_count;
         stack_push(self->allocator->stack, node);
     }
 
