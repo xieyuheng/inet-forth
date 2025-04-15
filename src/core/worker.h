04a0447ad9f155fb8cd72e780f8b4d5be6c250f6
Xie Yuheng <xyheme@gmail.com>
2025-04-15 20:11:07 +0800

    up

--- src/core/worker.h
+++ src/core/worker.h
@@ -19,13 +19,13 @@ struct worker_t {
 worker_t *worker_new(mod_t *mod, node_allocator_t *node_allocator);
 void worker_destroy(worker_t **self_pointer);
 
-void worker_print_return_stack(const worker_t *self, file_t *file);
-void worker_print_value_stack(const worker_t *self, file_t *file);
-void worker_print(const worker_t *self, file_t *file);
-
 void worker_connect(worker_t *self, value_t left, value_t right);
 
 node_t *worker_add_node(worker_t* self, const node_ctor_t *ctor);
 void worker_delete_node(worker_t* self, node_t *node);
 
 node_t *worker_lookup_node_by_wire(worker_t* self, wire_t *wire);
+
+void worker_print_return_stack(const worker_t *self, file_t *file);
+void worker_print_value_stack(const worker_t *self, file_t *file);
+void worker_print(const worker_t *self, file_t *file);
